279ca6f7a13ca8726751890c1742362b2c860c1c
Dominik Haentsch <dominik.haentsch@student.kit.edu>
2017-10-25 15:03:45 +0200

    removed old build files

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/students/haentsch/src/sofa/applications/plugins/MyPlugin
-# Build directory: /home/students/haentsch/src/sofa/build/applications/plugins/MyPlugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
